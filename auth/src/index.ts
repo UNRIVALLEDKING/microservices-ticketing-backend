6c72076425dc86a5a847254c297bba12411f35d5
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-09-16 12:48:40 +0530

    index

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,8 @@ import { signInRouter } from './routes/signin';
 import { signUpRouter } from './routes/signup';
 import { signOutRouter } from './routes/signout';
 
+import { errorHandler } from './middlewares/error-handler';
+
 const app = express();
 
 app.use(json());
@@ -14,6 +16,8 @@ app.use(currentUserRouter);
 app.use(signUpRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Yahallo!');
